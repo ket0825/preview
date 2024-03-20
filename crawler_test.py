0eea42f85e583d147ab703c594be8a8e5e6b7ba3
KET <ket0825@gmail.com>
2024-03-21 04:03:59 +0900

    [MODIFIEDD] Crawler testing modified.

--- crawler_test.py
+++ crawler_test.py
@@ -1,89 +1,69 @@
-
-import selenium
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
-from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
+# 3rd party lib.
+from driver.driver import Driver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
-options = Options()
-## Option 목록.
-# #지정한 user-agent로 설정합니다.
-# user_agent = "Mozilla/5.0 (Linux; Android 9; SM-G975F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.83 Mobile Safari/537.36"
-# options.add_argument('user-agent=' + user_agent)
+# custom lib.
+from log import Logger 
+log = Logger.get_instance()
 
-# options.add_argument('headless') #headless모드 브라우저가 뜨지 않고 실행됩니다.
-# options.add_argument('--window-size= x, y') #실행되는 브라우저 크기를 지정할 수 있습니다.
-# options.add_argument('--start-maximized') #브라우저가 최대화된 상태로 실행됩니다.
-# options.add_argument('--start-fullscreen') #브라우저가 풀스크린 모드(F11)로 실행됩니다.
-# options.add_argument('--blink-settings=imagesEnabled=false') #브라우저에서 이미지 로딩을 하지 않습니다.
-# options.add_argument('--mute-audio') #브라우저에 음소거 옵션을 적용합니다.
-# options.add_argument('incognito') #시크릿 모드의 브라우저가 실행됩니다.
+def test():
+    category = 'smartwatch'
+    naver_shopping_driver = Driver(headless=False)
 
-
-"""
-언제까지 웹드라이버가 기다릴지.
-https://selenium-python.readthedocs.io/waits.html
-"""
-
-"""
-각 종 find 참고.
-https://www.geeksforgeeks.org/find_element_by_xpath-driver-method-selenium-python/
-"""
-# WebDriverWait(self.driver, time).until(EC.element_to_be_clickable((By.CLASS_NAME, class_name)))
-class Driver:
-
-    def __init__(self, headless=True) -> None:
-        options = Options()
-        if headless:
-            options.add_argument("--headless=new")
-        else:
-            options.headless = False
-        self.driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()),
-                                       options=options)
-        self.headless = headless
-
-    def get(self, url:str) -> None:
-        self.driver.get(url)
-
-    def get_url_by_category(self, category:str) -> None:
-        if category == 'smartwatch':
-            self.driver.get("https://search.shopping.naver.com/search/category/100005046?adQuery&catId=50000262&origQuery&pagingIndex=1&pagingSize=40&productSet=model&query&sort=rel&spec=M10016843%7CM10664435&timestamp=&viewType=list")
-
-    def wait(self, time:float) -> None:
-        self.driver.implicitly_wait(time_to_wait=time)
+    # 태그 매치
+    """https://saucelabs.com/resources/blog/selenium-tips-css-selectors"""
+    # XPATH 예시들.
+    """https://selenium-python.readthedocs.io/locating-elements.html"""
+    # XPATH substring match.
+    # link = naver_shopping_driver.driver.find_elements(By.XPATH, "//div[contains(@class, 'product_title_')/a]") 
+    # CSS_SELECTOR substring match.
+    # link = naver_shopping_driver.driver.find_elements(By.CSS_SELECTOR, "div[class*='reviewItems_title']") # substring match.
     
-    def wait_until_by_css_selector(self, time:float, class_name:str) -> None:
-        WebDriverWait(self.driver, time).until(EC.element_to_be_clickable((By.CSS_SELECTOR, class_name)))
+    naver_shopping_driver.get_url_by_category(category)
+    naver_shopping_driver.wait(5)
+    # # username = driver.find_element(By.XPATH, "//form[input/@name='username']")
+    a_tags = naver_shopping_driver.driver.find_elements(By.XPATH, "//div[contains(@class, 'product_title_')]/a[@href]") # substring match.
     
-    def click(self):
-        self.driver.click()
+    for a_tag in a_tags:
+        log.info(f"link: {a_tag.get_attribute('href')}")
+
+    naver_shopping_driver.go_next_page()
+    a_tags = naver_shopping_driver.driver.find_elements(By.XPATH, "//div[contains(@class, 'product_title_')]/a[@href]") # substring match.
+    for a_tag in a_tags:
+        log.info(f"link: {a_tag.get_attribute('href')}")
+
+    # 리뷰 쿼리:"https://search.shopping.naver.com/api/review?isNeedAggregation=N&nvMid=42620727618&page=4&pageSize=20&sortType=RECENT"
+    # TODO: network 확인.
+    # headers = {
+    # "Accept": "application/json, text/plain, */*",
+    # "Accept-Encoding": "gzip, deflate, br",
+    # "Accept-Language": "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7",
+    # "Cookie": "",  # 실제 쿠키 값 입력
+    # "Dnt": "1",
+    # "Referer": "https://search.shopping.naver.com/catalog/41438134618?&NaPm=ct%3Dlu03bvig%7Cci%3Dd5bc94763ffed9c534d4d2b0f3651f852e00ac05%7Ctr%3Dslcc%7Csn%3D95694%7Chk%3Dcf893e9e08f5a7ed5c57998bf3eaeab7a5c2a324",
+    # "Sbth": "c4d56949d3f08269430ef301c81e1797c17a5c3b4a3156aa5f4ef53a7a06db25d2746a713596986a2824d2736e145db4",
+    # "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"
+    # }
+
+    # # Chrome DevTools Protocol 명령 실행
+    # cmd = "Network.setExtraHTTPHeaders"
+    # params = {"headers": headers}
+    # naver_shopping_driver.driver.execute_cdp_cmd(cmd, params)
+
+    # url = "https://search.shopping.naver.com/api/review?isNeedAggregation=N&nvMid=41438134618&page=1&pageSize=20&sortType=RECENT"
+    # response = naver_shopping_driver.driver.execute_cdp_cmd("Network.sendRequestWithContext", {"url": url, "method": "GET"})
+
+    # # 응답 처리
+    # if response.get("success"):
+    #     body = response.get("response", {}).get("body", "")
+    #     print(body)
+    # else:
+    #     error = response.get("message")
+    #     print(f"Error: {error}")
+
+    naver_shopping_driver.release()
+
+
+if __name__ == '__main__':
+    test()
     
-    # def find_element_by_xpath(self, element, element_name):
-    #     """
-    #     XML 형식에 따라 parsing함. 궁금하다면 XML 확인.
-    #     """
-    #     self.driver.find_element(By.XPATH, "f{element}")
-
-    def release(self):
-        self.driver.quit()
-        print("DRIVER CLOSING...")
-
-
-
-
-        
-
-category = 'smartwatch'
-naver_shopping_driver = Driver(headless=False)
-
-# naver_shopping_driver.wait(20)
-naver_shopping_driver.get_url_by_category(category)
-# naver_shopping_driver.wait_until_by_css_selector(10, "a[href*='product_link']")
-naver_shopping_driver.wait(5)
-link = naver_shopping_driver.driver.find_element(By.CSS_SELECTOR, "a[href*='product_link']")
-print(link)
-
-naver_shopping_driver.release()
