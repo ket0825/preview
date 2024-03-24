d16de7272c16eb71a15f82539dd643f4deef1f19
KET <ket0825@gmail.com>
2024-03-25 02:47:49 +0900

    [MODIFIED] Driver which mix the user-agent list NEXT: Proxy server needs.

--- driver/driver.py
+++ driver/driver.py
@@ -11,6 +11,13 @@ from selenium.webdriver import ActionChains
 from random_user_agent.user_agent import UserAgent
 from random_user_agent.params import SoftwareName, OperatingSystem, SoftwareEngine, HardwareType, Popularity, SoftwareType
 
+import random
+
+#TODO: proxy ip list need.
+"""
+프록시 서버 필요함... 아래 링크 참조.
+https://jaehyojjang.dev/python/free-proxy-server/
+"""
 
 from log import Logger
 log = Logger.get_instance()
@@ -56,21 +63,18 @@ class Driver:
         options = Options()
         if headless:
             options.add_argument("--headless=new")
-        
-        software_names = [SoftwareName.CHROME.value]
-        operating_systems = [OperatingSystem.WINDOWS.value]   
-        HardwareType.COMPUTER.value
-        
+               
         user_agent_rotator = UserAgent(
             hardware_types=HardwareType.COMPUTER.value,
             software_types=SoftwareType.WEB_BROWSER.value,
             software_names=SoftwareName.CHROME.value, 
             operating_systems=OperatingSystem.WINDOWS.value,
             popularity=Popularity.POPULAR.value,
-            limit=100
+            limit=1000
             )
         # # Get list of user agents.
         user_agents = user_agent_rotator.get_user_agents()
+        random.shuffle(user_agents)
         user_agent = ""
         for ua in user_agents:
             if "Windows NT 10.0" in ua['user_agent']:
