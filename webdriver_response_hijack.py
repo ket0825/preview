b032b8bf9d4fecdab8fe20eb0ea2cc7e421efd1a
KET <ket0825@gmail.com>
2024-03-25 02:49:38 +0900

    [ADDED] Response hijack completed.

--- webdriver_response_hijack.py
+++ webdriver_response_hijack.py
@@ -6,12 +6,9 @@ import random
 from driver.driver import Driver
 from selenium.webdriver.common.by import By
 
-
 # custom lib.
 from log import Logger 
 log = Logger.get_instance()
-
-xhr_pattern = r"XMLHttpRequest"
 
 def test():
     category = 'smartwatch'
@@ -22,7 +19,7 @@ def test():
     # XPATH 예시들.
     """https://selenium-python.readthedocs.io/locating-elements.html"""
 
-    # 아이폰
+    # 원래는 크롤링한 사이트 링크들.
     naver_shopping_driver.get("https://search.shopping.naver.com/catalog/42617437619?&NaPm=ct%3Dlu1a619c%7Cci%3D9a989f171d581b33e94796aa84f47af8e526ac37%7Ctr%3Dslcc%7Csn%3D95694%7Chk%3Db1354d632187303e9f2c56687a8afd0ad88ef9b0")
     # 디알고 헤드셋.
     # naver_shopping_driver.get("https://search.shopping.naver.com/catalog/36974003618?adId=nad-a001-02-000000223025435&channel=nshop.npla&cat_id=%EB%94%94%EC%A7%80%ED%84%B8/%EA%B0%80%EC%A0%84&NaPm=ct%3Dlu2l3ulc%7Cci%3D0zW0003ypdPztN%5FoFfjw%7Ctr%3Dpla%7Chk%3Dc6b52bbfde6b3967102cd5b772f10fb97a2d3356&cid=0zW0003ypdPztN_oFfjw")
@@ -42,33 +39,29 @@ def test():
     
     while not is_last_page:
         review_section = naver_shopping_driver.driver.find_element(By.XPATH, "//div[contains(@class, 'review_section_review')]")
-        review_pagination = review_section.find_element(By.XPATH, ".//div[contains(@class, 'pagination_pagination')]")
-        review_pages = review_pagination.find_elements(By.XPATH, ".//a")
-        x, y = review_section.find_elements(By.XPATH, ".//div[contains(@class, 'reviewItems_review')]")[-1].location
-        
-        for page in review_pages:
-            
+        # review_pagination = review_section.find_element(By.XPATH, ".//div[contains(@class, 'pagination_pagination')]/a")
+        review_pages = review_section.find_elements(By.XPATH, ".//div[contains(@class, 'pagination_pagination')]/a")        
+
+        if 'next' not in review_pages[-1].get_attribute('class'): # next라는게 없을 때까지 -> 리뷰의 끝까지.
+            is_last_page = True
+
+        for review_page in review_pages:
+            # move_to_marker = review_section.find_elements(By.XPATH, ".//div[contains(@class, 'reviewItems_btn_area')]")[-1]
             if page_num == 90:
                 log.info(review_pages)
-            is_current_page = 'now' in page.get_attribute('class')
-            is_prev_pages_button = 'prev' in page.get_attribute('class')
+            is_current_page = 'now' in review_page.get_attribute('class')
+            is_prev_pages_button = 'prev' in review_page.get_attribute('class')
             
             if is_prev_pages_button or is_current_page:
                 continue
             
             log.info(f"[INFO] Current page_num: {page_num}")
-            naver_shopping_driver.driver.execute_script("scrollTo(arguments[0],arguments[1])", x, y)
     
-            # naver_shopping_driver.move_to_element(element=move_to_marker)
+            naver_shopping_driver.move_to_element(element=review_page)
+            review_page.click()
             time.sleep(random.randint(2,6)*0.5)
-            page.click()
+            
             page_num+=1
-
-        
-
-        if 'next' not in review_pages[-1].get_attribute('class'):
-            is_last_page = True
-
         
     time.sleep(1) # 기다려야 log가 수집됨.
     
