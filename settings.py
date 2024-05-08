44cfa5e8f8322a437a6acc61a78fac02d4723283
KET <ket0825@gmail.com>
2024-05-08 19:40:37 +0900

    [ADDED] route_handler

--- settings.py
+++ settings.py
@@ -1,3 +1,14 @@
 import logging
+import os
+from dotenv import load_dotenv
 
 log_level = logging.INFO
+load_dotenv(dotenv_path='env/.env')
+STAGE = os.getenv('STAGE') if os.getenv('STAGE') else "local" 
+
+if STAGE == 'local':
+    URL = os.getenv('URL') if os.getenv('URL') else "http://localhost:5000/api"
+elif STAGE == 'dev':
+    URL = os.getenv('DEV_URL')
+elif STAGE == 'prod':
+    URL = os.getenv('PROD_URL')
